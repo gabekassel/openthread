8835eac5dfbc672d132d7391cbbe8ff6b88457d9
Gabe Kassel <gabekas@amazon.com>
2022-03-24 10:43:48 -0700

    change default

--- src/posix/main.c
+++ src/posix/main.c
@@ -385,7 +385,7 @@ int main(int argc, char *argv[])
     }
 
     ParseArg(argc, argv, &config);
-    openlog(argv[0], LOG_PID | (config.mIsVerbose ? LOG_PERROR : 0), OPENTHREAD_POSIX_CONFIG_SYSLOG_FACILITY_ID);
+    openlog(argv[0], LOG_PID | (config.mIsVerbose ? LOG_PERROR : 0), LOG_LOCAL5);
     setlogmask(setlogmask(0) & LOG_UPTO(LOG_DEBUG));
     instance = InitInstance(&config);
 
